10f6d3a733a0e62daa1f915c7467a0a029338e08
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-01-20 10:54:21 +0100

    Proxy server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello')
+var httpProxy = require('http-proxy');
+var http = require('http');
+var morgan = require('morgan');
+var logger = morgan('short');
+var proxy = httpProxy.createProxyServer({
+    target: 'https://accountview.net',
+    secure: false
+  })
+  .on('proxyRes', function (proxyRes, req, res) {
+    proxyRes.headers['access-control-allow-origin'] = '*'
+  });
+
+var listener = http.createServer(
+  function (req, res) {
+    logger(req, res, function (err) {
+      // if (err) return done(err)
+      proxy.web(req, res);
+    })
+  }
+).listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
